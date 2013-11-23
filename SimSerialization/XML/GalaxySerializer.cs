e3dce7e850ea33827078fe910fa2b01657e5317b
Jeffery Myers <JeffM2501@gmail.com>
2013-11-22 18:05:31 -0800

    Start Serializer proejct

---
+++ SimSerialization/XML/GalaxySerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+using SimCore;
+using SimCore.Universe;
+
+namespace SimSerialization.XML
+{
+    public class GalaxySerializer
+    {
+    }
+}
---
+++ SimSerialization/XML/SectorSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+using SimCore;
+using SimCore.Universe;
+
+namespace SimSerialization.XML
+{
+    public class SectorDefinition
+    {
+
+    }
+
+    public class SectorSerializer
+    {
+    }
+}
