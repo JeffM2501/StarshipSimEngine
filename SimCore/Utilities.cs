bea7e2161cec67a9092e35544fc41ae50d5795a8
Jeffery Myers <JeffM2501@gmail.com>
2013-12-03 16:45:27 -0800

    Tools to get all the systems that are in the sim assembly

---
+++ SimCore/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+
+using SimCore.Data.Systems;
+
+namespace SimCore.Utilities
+{
+    public static class Utils
+    {
+        public static List<Type> GetSystemTypes()
+        {
+            List<Type> types = new List<Type>();
+
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (t.IsSubclassOf(typeof(BaseSystem)))
+                    types.Add(t);
+            }
+
+            types.Add(typeof(BaseSystem));
+
+            return types;
+        }
+    }
+}
